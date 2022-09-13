c04a54ad1b717af2ba1aaa45dedf20034c8903e9
Zakariafahim334 <108035296+Zakariafahim334@users.noreply.github.com>
2022-09-13 03:18:40 +0100

    Add files via upload

---
+++ QR-CODE.js
@@ -0,0 +1,24 @@
+const wrapper1 = document.querySelector(".wrapper1"),
+qrInput = wrapper1.querySelector(".form input"),
+generateBtn = wrapper1.querySelector(".form button"),
+qrImg = wrapper1.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper1.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper1.classList.remove("active");
+        preValue = "";
+    }
+});
